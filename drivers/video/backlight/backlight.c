92342bb304d558a385fd2a5b1fe1c30745775039
Oren Laadan <orenl@cellrox.com>
2013-08-21 23:46:21 -0400

    dev_ns: adopt the backlight lcd subsystem
    Adds device namespace awareness to backlight and lcd subsystem - so that
    changes to the brightness/power/contrast setttings can only be made from an
    active namespace.
    
    Changelog:
    [16-Aug-2013] v1 - initial version
    
    Change-Id: I5ba918f31d66578a2145200b7f6fdeb842d7ec11
    Signed-off-by: Oren Laadan <orenl@cellrox.com>

--- drivers/video/backlight/backlight.c
+++ drivers/video/backlight/backlight.c
@@ -16,6 +16,7 @@
 #include <linux/err.h>
 #include <linux/fb.h>
 #include <linux/slab.h>
+#include <linux/dev_namespace.h>
 
 #ifdef CONFIG_PMAC_BACKLIGHT
 #include <asm/backlight.h>
@@ -123,6 +124,14 @@ static ssize_t backlight_store_power(struct device *dev,
 		return rc;
 
 	rc = -ENXIO;
+
+	if (!is_active_dev_ns(current_dev_ns())) {
+		printk(KERN_INFO "%s: not setting %s power to %ld from inactive devns\n",
+		       __func__, dev_name(dev), power);
+		rc = count;
+		goto out;
+	}
+
 	mutex_lock(&bd->ops_lock);
 	if (bd->ops) {
 		pr_debug("set power to %lu\n", power);
@@ -134,6 +143,7 @@ static ssize_t backlight_store_power(struct device *dev,
 	}
 	mutex_unlock(&bd->ops_lock);
 
+ out:
 	return rc;
 }
 
@@ -158,6 +168,16 @@ static ssize_t backlight_store_brightness(struct device *dev,
 
 	rc = -ENXIO;
 
+	if (!is_active_dev_ns(current_dev_ns())) {
+		printk(KERN_INFO "%s: not setting %s brightness to %ld from inactive devns\n",
+		       __func__, dev_name(dev), brightness);
+		rc = count;
+		/* generate the backlight event in case user space was relying
+		 * on this to happen
+		 */
+		goto out;
+	}
+
 	mutex_lock(&bd->ops_lock);
 	if (bd->ops) {
 		if (brightness > bd->props.max_brightness)
@@ -173,6 +193,7 @@ static ssize_t backlight_store_brightness(struct device *dev,
 
 	backlight_generate_event(bd, BACKLIGHT_UPDATE_SYSFS);
 
+ out:
 	return rc;
 }
 
