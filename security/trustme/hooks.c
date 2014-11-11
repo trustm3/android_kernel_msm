bfb16bc678287c09b2c42781d97fbd7a95b502b5
Julian Horsch <julian.horsch@aisec.fraunhofer.de>
2014-11-11 12:41:08 +0100

    Added initial almost empty trustme LSM
    Change-Id: I8fd51bdb0748fc9f3f9a3f2f067ef6c9de584b11

---
+++ security/trustme/hooks.c
@@ -0,0 +1,38 @@
+#include <linux/security.h>
+
+static int trustme_binder_set_context_mgr(struct task_struct *mgr)
+{
+	printk(KERN_INFO "trustme-lsm: binder_set_context_mgr called");
+	return 0;
+}
+
+static int trustme_capable(const struct cred *cred, struct user_namespace *ns,
+			int cap, int audit)
+{
+	//printk(KERN_INFO "trustme-lsm: trustme_capable hook called");
+	return 0;
+}
+
+static struct security_operations trustme_ops = {
+	.name =			"trustme",
+
+	.binder_set_context_mgr = trustme_binder_set_context_mgr,
+	.capable = trustme_capable,
+};
+
+static __init int trustme_init(void)
+{
+	/* Normally a module should call this, but we don't, because we want to be 
+	 * active in any case and we don't interfere with other modules (no xattr etc.)*/
+//	if (!security_module_enable(&trustme_ops))
+//		return 0;
+
+	printk(KERN_INFO "trustme-lsm: becoming mindful.\n");
+
+	if (register_security(&trustme_ops))
+		panic("trustme-lsm: kernel registration failed.\n");
+
+	return 0;
+}
+
+security_initcall(trustme_init);
