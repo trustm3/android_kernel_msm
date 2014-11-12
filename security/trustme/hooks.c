64b9ce39a082f0085725a507645f4dc63dce6eb7
Julian Horsch <julian.horsch@aisec.fraunhofer.de>
2014-11-12 14:59:45 +0100

    trustme-lsm: added some sample code
    Change-Id: Ic7693ca08c68a495d7e8e34aee9408f2a84df402

--- security/trustme/hooks.c
+++ security/trustme/hooks.c
@@ -1,15 +1,67 @@
 #include <linux/security.h>
+#include <linux/path.h>
+#include <linux/fs_struct.h>
+#include <linux/fs.h>
+#include <linux/mount.h>
+#include <linux/dcache.h>
+
+#include <linux/pid_namespace.h>
 
 static int trustme_binder_set_context_mgr(struct task_struct *mgr)
 {
+	struct pid_namespace *pidns = task_active_pid_ns(mgr);
 	printk(KERN_INFO "trustme-lsm: binder_set_context_mgr called");
+
+	printk(KERN_INFO "trustme-lsm: new context manager %s with pid: %d and vpid: %d",
+			mgr->comm, task_pid_nr(mgr), task_pid_vnr(mgr));
+	printk(KERN_INFO "trustme-lsm: child reaper of pidns: %s with pid %d",
+			pidns->child_reaper->comm, task_pid_nr(pidns->child_reaper));
+
+	return 0;
+}
+
+static int trustme_binder_transaction(struct task_struct *from, struct task_struct *to)
+{
+	//printk(KERN_INFO "binder transaction: from: %s to: %s", from->comm, to->comm);
+
+	/* prevent binder transactions over container boundaries */
+	if (task_active_pid_ns(from) != task_active_pid_ns(to)) {
+		printk(KERN_INFO "trustme-lsm: deny inter-container binder communication");
+		return -1;
+	}
+	return 0;
+}
+
+static int trustme_inode_permission(struct inode *inode, int mask)
+{
+	//struct pid_namespace *pidns = task_active_pid_ns(current);
+
+	// TODO check the policy if the pidns may access the inode...
+	/*
+	struct pid_namespace *pidns = task_active_pid_ns(current);
+	pid_t container_init_pid = task_pid_nr(pidns->child_reaper);
+	struct super_block *current_root_sb = current->fs->root.mnt->mnt_sb;
+	struct super_block *inode_sb = inode->i_sb;
+	if (current_root_sb != inode_sb) {
+		printk(KERN_INFO "trustme-lsm: container (PID: %d on superblock %s) accessing"
+				" inode on superblock %s",
+				container_init_pid,
+				current_root_sb->s_id,
+				inode_sb->s_id);
+	}
+	*/
+
+	return 0;
+}
+
+static int trustme_file_permission(struct file *file, int mask)
+{
 	return 0;
 }
 
 static int trustme_capable(const struct cred *cred, struct user_namespace *ns,
 			int cap, int audit)
 {
-	//printk(KERN_INFO "trustme-lsm: trustme_capable hook called");
 	return 0;
 }
 
@@ -17,6 +69,9 @@ static struct security_operations trustme_ops = {
 	.name =			"trustme",
 
 	.binder_set_context_mgr = trustme_binder_set_context_mgr,
+	.binder_transaction = trustme_binder_transaction,
+	.inode_permission = trustme_inode_permission,
+	.file_permission = trustme_file_permission,
 	.capable = trustme_capable,
 };
 
